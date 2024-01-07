cf280813e4c475ab714539651deadf630cca8feb
Yash9891 <yash042002@gmail.com>
2024-01-07 19:01:53 +0530

    commit

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const MongoDBStore = require('connect-mongo');
 
 
 
-const dbUrl=process.env.DATABASE
+const dbUrl=process.env.DATABASE || "mongodb://127.0.0.1:27017/yelp-camp"
 mongoose.connect(dbUrl);
 const db=mongoose.connection;
 db.on("error",console.error.bind(console,"Connection Error"));
